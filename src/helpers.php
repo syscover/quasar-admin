c1ac677907e9dab68af08680f72b829e7ea0093e
Carlos Rodríguez Palacín <carlos.rodriguez@techedgegroup.es>
2020-01-13 19:53:19 +0100

    add base_lang helper function

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,14 @@
 <?php
+
+if (! function_exists('base_lang'))
+{
+    /**
+     * Get base lang object from config file.
+     *
+     * @return string
+     */
+    function base_lang()
+    {
+        return config('quasar-admin.base_lang');
+    }
+}

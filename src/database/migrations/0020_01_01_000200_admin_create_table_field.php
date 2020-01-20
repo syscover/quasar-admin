2b57da59ea572d52095fc91859a49ada2847404e
Carlos <carlos.rodriguez@techedgegroup.com>
2020-01-21 00:34:08 +0100

    migrations

---
+++ src/database/migrations/0020_01_01_000200_admin_create_table_field.php
@@ -0,0 +1,79 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AdminCreateTableField extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(! Schema::hasTable('admin_field'))
+        {
+            Schema::create('admin_field', function (Blueprint $table) {
+                $table->engine = 'InnoDB';
+                
+                $table->increments('id');
+                $table->uuid('uuid');
+                $table->uuid('field_group_uuid');
+                $table->string('name')->nullable();
+                $table->json('labels')->nullable();                     // label value in different languages
+
+                $table->uuid('field_type_uuid');                        // see config/quasar-admin.php
+                $table->uuid('source_uuid')->nullable();
+                $table->string('field_type_name');
+                // 1 - Text
+                // 2 - Select
+                // 3 - Select multiple
+                // 4 - Number
+                // 5 - Email
+                // 6 - Checkbox
+                // 7 - Select 2
+                // 8 - Select 2 multiple
+                // 9 - Text area
+                // 10 - Wysiwyg
+
+                $table->uuid('data_type_id');                               // see config/pulsar-admin.php
+                $table->string('data_type_name');
+                // 1 - String
+                // 2 - Boolean
+                // 3 - Integer
+                // 4 - Float
+                // 5 - Array
+                // 6 - Object
+
+                $table->boolean('required')->default(false);
+                $table->smallInteger('sort')->unsigned()->nullable();
+                $table->integer('max_length')->unsigned()->nullable();
+                $table->string('pattern')->nullable();
+                $table->string('class')->nullable();                    // class style for component
+                $table->json('data_lang')->nullable();                  // set different langs in json
+                $table->json('data')->nullable();
+
+                $table->timestamps();
+                $table->softDeletes();
+
+                $table->index('uuid', 'admin_field_uuid_idx');
+                $table->foreign('field_group_uuid', 'admin_field_field_group_uuid_fk')
+                    ->references('uuid')
+                    ->on('admin_field_group')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_field');
+    }
+}
---
+++ src/database/migrations/0020_01_01_000210_admin_create_table_field_value.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AdminCreateTableFieldValue extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(! Schema::hasTable('admin_field_value'))
+        {
+            Schema::create('admin_field_value', function (Blueprint $table) {
+                $table->engine = 'InnoDB';
+
+                $table->increments('id');
+                $table->uuid('uuid');
+                $table->string('code', 50);
+
+                // counter to assign number to id if has not ID
+                $table->integer('counter')->unsigned()->nullable();
+                $table->uuid('lang_uuid');
+                $table->uuid('field_uuid');
+                $table->string('name');
+                $table->smallInteger('sort')->unsigned()->nullable();
+                $table->boolean('featured')->default(false);
+                $table->json('data_lang')->nullable();
+                $table->json('data')->nullable();
+
+                $table->timestamps();
+                $table->softDeletes();
+
+                $table->index('uuid', 'admin_field_value_uuid_idx');
+                $table->index('code', 'admin_field_value_code_idx');
+                $table->unique(['code', 'lang_uuid', 'field_uuid'], 'admin_field_value_uuid_lang_uuid_field_uuid_uq');
+                $table->foreign('lang_uuid', 'admin_field_value_lang_uuid_fk')
+                    ->references('uuid')
+                    ->on('admin_lang')
+                    ->onDelete('restrict')
+                    ->onUpdate('cascade');
+                $table->foreign('field_uuid', 'admin_field_value_field_uuid_fk')
+                    ->references('uuid')
+                    ->on('admin_field')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_field_value');
+    }
+}
